b65474fc2901bb004adc53697081670c7faf63b8
Anish Joshi <anishjoshi2056@hotmail.com>
2021-04-24 14:03:31 +0545

    Section 406

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const Campground = require('./models/campground');
+const mongoose = require('mongoose');
+//Connection with MongoDB
+
+mongoose.connect('mongodb://localhost:27017/yelpcamp',{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+});
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=> {
+    console.log("database connected");
+})
 //Setting view engine template
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+//Routes
 app.get('/',(req,res)=> {
     res.render('home')
 })
-
+app.get('/makecampground',async (req,res)=> {
+    const camp = new Campground({title:"My background",description:"Cheap camping!",price:13000});
+    await camp.save();
+    res.send(camp);
+})
 app.listen(port,()=> {
     console.log(`Listining to the port:${port}`)
 })
