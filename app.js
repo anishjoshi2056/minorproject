f489ca090a958ee6fd457861a4661c416c6d1cd4
Anish Joshi <anishjoshi2056@hotmail.com>
2021-04-30 07:59:25 +0545

    Campground new and create

--- app.js
+++ app.js
@@ -20,25 +20,34 @@ db.once("open",()=> {
 //Setting view engine template
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+//To parse the req.body
+app.use(express.urlencoded({extended:true}))
 //Routes
 app.get('/',(req,res)=> {
     res.render('home')
 })
-// app.get('/makecampground',async (req,res)=> {
-//     const camp = new Campground({title:"My background",description:"Cheap camping!",price:13000});
-//     await camp.save();
-//     res.send(camp);
-// })
 
 app.get('/campgrounds', async(req,res)=>{
     const campgrounds = await Campground.find({})
-    console.log(campgrounds)
     res.render('campgrounds/index',{campgrounds}) 
+})
+app.get('/campgrounds/new',(req,res)=> {
+    res.render('campgrounds/new')
+})
+app.post('/campgrounds',async(req,res) => {
+    /* req.body is empty so we have to tell 
+    express to parse the body */
+    const campground = new Campground(req.body.campground);
+    console.log(campground);
+    //saving to the database
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 app.get('/campgrounds/:id',async(req,res)=> {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show',{campground})
 })
+
 app.listen(port,()=> {
     console.log(`Listining to the port:${port}`)
 })
